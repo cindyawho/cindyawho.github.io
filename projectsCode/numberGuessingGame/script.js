138db3ff6cca43d2d363b1c9cc9478f6fbb71b6c
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-04-15 21:00:07 -0700

    Table cells are produced, need to fix numbers being passed through, and fix how to update on each attempt

--- projectsCode/numberGuessingGame/script.js
+++ projectsCode/numberGuessingGame/script.js
@@ -76,10 +76,26 @@ function gameResult(gameReturn){
 function updateTable(attemptsMade, totalGuesses, numGuessed){
     const tableElem = document.getElementById("gameTable");
     const attemptRow = tableElem.childNodes[1].childNodes[0];
-    console.log(attemptRow);
+    // console.log(attemptRow);
     const numGuessRow = tableElem.childNodes[1].childNodes[2];
-    console.log(numGuessRow);
-    console.log("Total Guesses: " + totalGuesses);
-    console.log("Number Guessed: " + numGuessed);
-    console.log("Table: " + tableElem + "\n");
+    // console.log(numGuessRow);
+    // console.log("Total Guesses: " + totalGuesses);
+    // console.log("Number Guessed: " + numGuessed);
+    // // console.log("Table: " + tableElem + "\n");
+    // const newAttempt = document.createElement("td");
+    // newAttempt.innerText = attemptsMade;
+    // document.attemptRow.appendChild(newAttempt);
+    // // create table cell for the number Player 2 guessed
+    // const newSecret = document.createElement("td");
+    // newSecret.innerText = numGuessed;
+    // document.numGuessRow.appendChild(newSecret);
+    
+
+    //create table row using W3: Table insertRow() Method
+    //New <td> element in attempts row
+    let newAttemptCell = attemptRow.insertCell(1);
+    newAttemptCell.innerHTML = attemptsMade;
+    //New <td> element in number guessed row
+    let newSecretCell = numGuessRow.insertCell(1);
+    newSecretCell.innerHTML = numGuessed;
 }
