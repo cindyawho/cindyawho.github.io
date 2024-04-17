0e4735476d38cb63dd0a5e56f4acc806da5f0d33
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-04-16 17:35:52 -0700

    Table is now updated in correct order with the correct values

--- projectsCode/numberGuessingGame/script.js
+++ projectsCode/numberGuessingGame/script.js
@@ -23,13 +23,13 @@ function playGuessingGame(numToGuess, totalGuesses){
             continue; // If the input is not a number, remind user to input number
         }
         else if(playerInput < numToGuess){
-            updateTable(i, totalGuesses, playerInput);
             playerInput = prompt(playerInput + " is too small. Guess a larger number.");
+            updateTable(i, totalGuesses, playerInput);
             continue;
         }
         else if(playerInput > numToGuess){
-            updateTable(i, totalGuesses, playerInput);
             playerInput = prompt(playerInput + " is too large. Guess a smaller number.");
+            updateTable(i, totalGuesses, playerInput);
             continue;
         }
         else if(playerInput == numToGuess){
@@ -73,29 +73,36 @@ function gameResult(gameReturn){
     document.getElementById("gameResultMessage").innerHTML = message;
 }
 
+function createTable(){ //for a new game
+    const tableElem = document.getElementById("gameTable");
+    tableElem.deleteRow(0);
+    tableElem.deleteRow(1);
+
+    let attemptRow = tableElem.insertRow(0);
+    th0 = document.createElement('th');
+    th0.innerHTML = "Attempt Number";
+    attemptRow.appendChild(th0);
+    
+    let numGuessRow = tableElem.insertRow(1);
+    th1 = document.createElement('th');
+    th1.innerHTML = "Number Guessed";
+    attemptRow.appendChild(th1);    
+}
+
 function updateTable(attemptsMade, totalGuesses, numGuessed){
     const tableElem = document.getElementById("gameTable");
     const attemptRow = tableElem.childNodes[1].childNodes[0];
-    // console.log(attemptRow);
+    console.log(attemptRow);
     const numGuessRow = tableElem.childNodes[1].childNodes[2];
-    // console.log(numGuessRow);
-    // console.log("Total Guesses: " + totalGuesses);
-    // console.log("Number Guessed: " + numGuessed);
-    // // console.log("Table: " + tableElem + "\n");
-    // const newAttempt = document.createElement("td");
-    // newAttempt.innerText = attemptsMade;
-    // document.attemptRow.appendChild(newAttempt);
-    // // create table cell for the number Player 2 guessed
-    // const newSecret = document.createElement("td");
-    // newSecret.innerText = numGuessed;
-    // document.numGuessRow.appendChild(newSecret);
-    
+    console.log(numGuessRow);
+    console.log("Total Guesses: " + totalGuesses);
+    console.log("Number Guessed: " + numGuessed);
 
     //create table row using W3: Table insertRow() Method
     //New <td> element in attempts row
-    let newAttemptCell = attemptRow.insertCell(1);
+    let newAttemptCell = attemptRow.insertCell(-1);
     newAttemptCell.innerHTML = attemptsMade;
     //New <td> element in number guessed row
-    let newSecretCell = numGuessRow.insertCell(1);
+    let newSecretCell = numGuessRow.insertCell(-1);
     newSecretCell.innerHTML = numGuessed;
 }
