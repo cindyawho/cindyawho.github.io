a6e6cc50211c09c2800223e11fce41b72c5122cb
cindyawho <161978639+cindyawho@users.noreply.github.com>
2024-11-11 11:19:52 -0800

    Create workExperiences.js
    Add some work experiences to data file

---
+++ data/workExperiences.js
@@ -0,0 +1,51 @@
+workExperiences = 
+[
+    {
+        "type": "technical",
+        "title" : "Snap Engineering Scholar",
+        "employer" : "Snap Inc.",
+        "dates" : "June 2024 - Aug 2024",
+        "descriptionHTML":"<p>During the Snap Engineering Academy Program, I worked on React projects, strengthened my professional development skills, and did multiple mock technical interviews.</p><p>As an engineering scholar, I...</p>",
+        "responsibilitiesHTML":"<ul class=\"workJobList\"><li>Accepted into a 15 person engineering academy with a 6% acceptance rate.</li><li>Pitched a final showcase project to 8 Snap executives and over 250 audience members. Engineered the prototype mobile app while working on a cross-functional team with engineers, designers and marketing. See <a href\"./projectsCode/giveFund/index.html\" target=\"_blank\">Give Fund Project</a> in portfolio.</li><li>Developed 5 projects with React and React Native, including an interactive game, chat system, and chatbot library.</li><span class=\"seeMoreLess\"><li>Participated in weekly professional development to support my personal and professional growth.</li><li>Formed a tight-knit community with Snap Engineering scholars and instructors where we supported and uplifted one another.</li></span><div class=\"seeFunction\">See More...</div></ul>"
+    },
+    {
+        "type": "technical other",
+        "title" : "HomeTeam GameDev Member",
+        "employer" : "HomeTeam GameDev",
+        "dates" : "June 2024",
+        "descriptionHTML":"<p>Learning game development programming through team projects led by other members. </p>",
+        "responsibilitiesHTML":"<ul class=\"workJobList\"><li>Exploring HTML5 Canvas with JavaScript, and game art.</li><li>Contributed to main menu art and code for two games.</li><li>See my progress in my HomeTeam <a href=\"https://hometeamgamedev.com/certificate.php?member=cindyawho\" target=\"_blank\">Character Sheet</a>.</li><li>For more information about the organization, visit the <a href=\"https://hometeamgamedev.com/apply.html\" target=\"_blank\">HomeTeam GameDev Website</a>.</li></ul>"
+    },
+    {
+        "type": "education",
+        "title" : "Mathematics Teacher",
+        "employer" : "Los Angeles Unified School District",
+        "dates" : "Aug 2019 - June 2024",
+        "descriptionHTML":"<p>I taught Math 6, Algebra I, and Geometry at Benjamin Franklin High School. My first three years I taught Geometry, Geometry Tutorial, Precalculus, Honors Advanced Math (Alg II + Precalculus), and AP Calculus AB at USC Media Arts and Engineering Magnet.</p><p>As a teacher I...</p>",
+        "responsibilitiesHTML":"<ul class=\"workJobList\"><li>Encourage partner discussions and check-ins while maintaining and encouraging a positive learning environment.</li><span class=\"seeMoreLess\"><li>Utilize various online learning platforms to enhance the learning of my students. Platforms include IXL, iReady, and Class Dojo. </li><li> Participated in the IB Authorization Team to provide support, discuss ideas, mentor teachers, plan IB-focused unit plans, and mentor 10th grade students with their IB personal projects. </li><li>Was a member of the WASC Writing Committee, where I updated sections of the WASC report in preparation for the Accreditation visit, gathered information from other school stakeholders, and revised the final report. </li><li>Currently lead the Chess Club and assist with the middle school Robotics club. Supervised the Coding Club and Senior Committee Club in 2021-2023. Clubs meet in my classroom every week after school or during lunch.</li><li>Prepare and host tutoring before and after school to help students make up missing assignments, study for assessments, and do assessment retakes.</li><li>Fundraised over $1000 for four different projects through DonorsChoose for calculators, student whiteboards, and art supplies for a Calculus Project.</li><li>Led math department meetings at my past school.</li></span><div class=\"seeFunction\">See More...</div></ul>"
+    },
+    {
+        "type": "education",
+        "title" : "Substitute Teacher",
+        "employer" : "Los Angeles Unified School District",
+        "dates" : "Jan 2020 - May 2020",
+        "descriptionHTML": "<p>As a substitute teacher I...</p>",
+        "responsibilitiesHTML":"<ul class=\"workJobList\"><li>Covered classes when teachers were unavailable. </li><span class=\"seeMoreLess\"><li>Enacted detailed lesson plans.</li> <li>Monitored the classroom to ensure student growth and safety.</li><li>Assisted students with their assignments in a caring manner.</li><li>Was flexible when assigned coverage for single periods in different classes or full days. </li></span><div class=\"seeFunction\">See More...</div></ul>"
+    },
+    {
+        "type": "other",
+        "title" : "Special Project Assistant",
+        "employer" : "USC Student Life",
+        "dates" : "Sept 2015 - May 2018",
+        "descriptionHTML":"<p>Note: El Centro Chicano is now known as <a href=\"https://lacasa.usc.edu/\">La CASA.</a></p><p>As a project assistant at El Centro Chicano, I...</p>",
+        "responsibilitiesHTML" : "<ul><li>Assisted with the management of various events, including the annual Latinx Graduate Celebration for about 100 graduates and over 300 guests; scheduled graduation pictures, confirmed sponsors, led rehearsals, and was on call throughout the event</li><span class=\"seeMoreLess\"><li>Welcomed students and visitors in a hospitable, professional demeanor to the center.</li><li>Answered calls with excellent phone etiquette and customer services skills while directing them to the proper party, and/or obtaining enough information to allow for follow-ups with visitors.</li><li>Created and edited flyers</li><li>Hosted visiting high school students by presenting about El Centro and did Q&As</li><li>Was Editing Manager for El Centro's weekly E-Newsletter.</li><li> Documented meeting notes, regularly communicated with stakeholders, managed over a dozen projects and events each year.</li></span><div class=\"seeFunction\">See More...</div></ul>"
+    },
+    {
+        "type": "ABCDEF",
+        "title" : "ABCDEF",
+        "employer" : "ABCDEF",
+        "dates" : "ABCDEF",
+        "descriptionHTML":"ABCDEF",
+        "responsibilitiesHTML":"ABCDEF"
+    }
+]
